8bc358cabb096d23618500ba7bffba85aba19596
清水俊樹 <tamreff3290@gmail.com>
2021-06-26 14:11:56 +0900

    15-13

--- ch15/Main.java
+++ ch15/Main.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         String s1 = "Java and JavaScript";
         String s2 = "Java";
         String s3 = "Java";
@@ -54,9 +58,44 @@ public class Main {
         String w = s.replaceAll("[beh]","X");
         System.out.println(w);
 
+        
         // 15-9
         System.out.println(String.format("製品番号%s-%02d", "SJV", 3));
         System.out.printf("製品番号%s-%02d", "SJV", 3);
         System.out.println();
+
+        // 15-10
+        long start = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+
+        // 15-11
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(now.getTime());
+        Date past = new Date(1600705425827L);
+        System.out.println(past);
+
+        // 15-12
+        Calendar c = Calendar.getInstance();
+        c.set(2017,8,22,1,23,45);
+        c.set(Calendar.MONTH, 9);
+        Date d = c.getTime();
+        System.out.println(d);
+
+        //Date now = new Date();
+        c.setTime(now);
+        int y = c.get(Calendar.YEAR);
+        System.out.println(y);
+
+        // 15-13
+        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date d1 = f.parse("2020/09/22 01:23:45");
+        System.out.println(d1);
+
+        Date now2 = new Date();
+        String ss = f.format(now2);
+        System.out.println(ss);
+
     }
 }
