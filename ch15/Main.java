1bd6a0636fc031e6916f2c2ed04c53848762c445
清水俊樹 <tamreff3290@gmail.com>
2021-06-26 11:08:54 +0900

    15-9

--- ch15/Main.java
+++ ch15/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        String s1 = "スッキリJava";
+        String s1 = "Java and JavaScript";
         String s2 = "Java";
         String s3 = "Java";
         if (s2.equals(s3)){
@@ -14,5 +14,49 @@ public class Main {
             System.out.println("s1 is empty.");
         }
 
+        if (s1.contains("Java")){
+            System.out.println("s1 contains Java");
+        }
+
+        if (s1.endsWith("Java")){
+            System.out.println("s1 endsWith Java");
+        }
+
+        System.out.println("s1 indexof Java :" + s1.indexOf("Java"));
+        System.out.println("s1 lastIndexOf Java :" + s1.lastIndexOf("Java"));
+
+        System.out.println(s1.substring(3));
+        System.out.println(s1.substring(3,8));
+        System.out.println(s1.replace("Ja","En"));
+
+        String s = "Java";
+        System.out.println(s.matches("Java"));
+        System.out.println(s.matches("JavaJava"));
+        System.out.println(s.matches("java"));
+
+        // 15-4
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<10000; i++){
+            sb.append("Java");
+        }
+        s = sb.toString();
+
+        // 15-7
+        s = "abc,def:ghi";
+        String[] words = s.split("[,:]");
+        for (String w : words){
+            System.out.print(w + "->");
+        }
+        System.out.println();
+
+        // 15-8
+        s = "abc,def:ghi";
+        String w = s.replaceAll("[beh]","X");
+        System.out.println(w);
+
+        // 15-9
+        System.out.println(String.format("製品番号%s-%02d", "SJV", 3));
+        System.out.printf("製品番号%s-%02d", "SJV", 3);
+        System.out.println();
     }
 }
