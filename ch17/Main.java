17d7bf4ea85c3f15ee4df7a2ae164b372ba8623c
清水俊樹 <tamreff3290@gmail.com>
2021-06-27 10:12:42 +0900

    ch17-13

--- ch17/Main.java
+++ ch17/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class Main{
     public static void main(String[] args){
+        // 17-9
         FileWriter fw = null;
         try{
             fw = new FileWriter("data.txt");
@@ -10,11 +11,32 @@ public class Main{
         } catch (Exception e){
             System.out.println("何らかの例外が発生しました");
         } finally{
-            try{
-                fw.close();
-            } catch (IOException e){
-                ;
+            if (fw != null) {
+                try{
+                    fw.close();
+                } catch (IOException e){
+                    ;
+                }
             }
+        }
+        // 17-10
+        try (FileWriter fw2 = new FileWriter("data.txt");){
+            fw2.write("hello!");
+        } catch (Exception e){
+            System.out.println("何らkなの例外が発生しました");
+        }
+
+        // 17-11
+        Person p = new Person();
+        //p.setAge(-128);
+        p.setAge(28);
+
+        // 17-12
+        try {
+            throw new UnsupportedMusicFlieException("未対応のファイルです");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
---
+++ ch17/UnsupportedMusicFlieException.java
@@ -0,0 +1,7 @@
+public class UnsupportedMusicFlieException extends Exception {
+    public UnsupportedMusicFlieException(String msg){
+        super(msg);
+    }
+}
+
+
