5d86a273ffc7c187d7ececcf4ee46993ee30d4cd
清水俊樹 <tamreff3290@gmail.com>
2021-06-27 14:17:59 +0900

    ch1-2

--- sukkiri-wakaru-java-nyumon-jissenhen/ch01/Main.java
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch01/Main.java
@@ -1,9 +1,19 @@
 public class Main{
     public static void main(String[] args){
+        // 1-1
         Hero h = new Hero();
         h.setName("ミナト");
         h.setHp(100);
         h.setMp(50);
         System.out.println(h);
+
+        // 1-2
+        Account a1 = new Account();
+        a1.accountNo = "001";
+        Account a2 = new Account();
+        a2.accountNo = "002";
+        System.out.println(a1.equals(a2));
+        a2.accountNo = " 001 ";
+        System.out.println(a1.equals(a2));
     }
 }
