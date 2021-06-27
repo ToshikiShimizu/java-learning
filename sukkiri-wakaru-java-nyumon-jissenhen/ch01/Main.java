a2a80449fe261e2d0d7226b66ba7671b9811e069
清水俊樹 <tamreff3290@gmail.com>
2021-06-27 13:22:14 +0900

    ch1-1

---
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch01/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        Hero h = new Hero();
+        h.setName("ミナト");
+        h.setHp(100);
+        h.setMp(50);
+        System.out.println(h);
+    }
+}
