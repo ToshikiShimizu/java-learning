a7aad2210c5310edb08ddd793c21a0ede3a1779a
清水俊樹 <tamreff3290@gmail.com>
2021-06-27 16:05:20 +0900

    ch01

---
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch01/practice/TitleComparator.java
@@ -0,0 +1,6 @@
+import java.util.Comparator;
+public class TitleComparator implements Comparator<Book>{
+    public int compare(Book x, Book y){
+        return x.getTitle().compareTo(y.getTitle());
+    }
+}
