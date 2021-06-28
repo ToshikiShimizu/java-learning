3df4b3dfba9c5a4a5a6465698bf9f6557565822b
清水俊樹 <tamreff3290@gmail.com>
2021-06-28 20:18:11 +0900

    4-3

--- sukkiri-wakaru-java-nyumon-jissenhen/ch04/Main.java
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch04/Main.java
@@ -1,6 +1,20 @@
+import java.util.*;
 public class Main{
     public static void main(String[] args){
-        System.exit(1);
-        System.out.println("正常終了しました");
+        // 4-1
+        // System.exit(1);
+        // System.out.println("正常終了しました");
+
+        // 4-3
+        System.out.print("利用中のJavaのバージョン:");
+        System.out.println(System.getProperty("java.version"));
+        Properties p = System.getProperties();
+        Iterator<String> i = p.stringPropertyNames().iterator();
+        System.out.println("システムプロパティ一覧");
+        while (i.hasNext()){
+            String key = i.next();
+            System.out.print(key + " = ");
+            System.out.println(System.getProperty(key));
+        }
     }
 }
