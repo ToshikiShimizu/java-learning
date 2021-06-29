d7836ced7158761d15294cf2e273457f85c91825
清水俊樹 <tamreff3290@gmail.com>
2021-06-29 11:16:08 +0900

    ch06-2

--- sukkiri-wakaru-java-nyumon-jissenhen/ch06/Main.java
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch06/Main.java
@@ -1,11 +1,25 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileReader;
 
 public class Main{
     public static void main(String[] args) throws IOException{
-        FileWriter fw = new FileWriter("./rpgsave.dat", true);
-        fw.write('A');
-        fw.flush();
+        // FileWriter fw = new FileWriter("./rpgsave.dat", true);
+        // 6-1
+        // fw.write('A');
+        // fw.flush();
+        // fw.close();
+
+        // 6-2
+        FileReader fw = new FileReader("./rpgsave.dat");
+        System.out.println("すべてのデータを読んで表示します");
+        int i = fw.read();
+        while (i!=-1){
+            char c = (char)i;
+            System.out.print(c);
+            i = fw.read();
+        }
+        System.out.println("ファイルの末尾に到達しました");
         fw.close();
     }
 }
