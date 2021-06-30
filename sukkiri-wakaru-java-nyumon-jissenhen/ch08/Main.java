7ea563cdf13637cb8d7c4798bbc75564815f1756
清水俊樹 <tamreff3290@gmail.com>
2021-06-30 21:58:51 +0900

    81

--- sukkiri-wakaru-java-nyumon-jissenhen/ch08/Main.java
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch08/Main.java
@@ -1,17 +1,45 @@
 import java.io.*;
 import java.net.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
-        URL url = new URL("https://dokojava.jp");
-        InputStream is = url.openStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        int i = isr.read();
-        while (i!=-1){
-            System.out.print((char)i);
-            i = isr.read();
+import java.net.ServerSocket;
+import java.net.Socket;
 
-        }
-    isr.close();
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // 8-1
+        // URL url = new URL("https://dokojava.jp");
+        // InputStream is = url.openStream();
+        // InputStreamReader isr = new InputStreamReader(is);
+        // int i = isr.read();
+        // while (i!=-1){
+        //     System.out.print((char)i);
+        //     i = isr.read();
+        //     isr.close();
+        // }
+        
+        // 8-2
+        // Socket sock = new Socket("dokojava.jp", 80);
+        // InputStream is = sock.getInputStream();
+        // OutputStream os = sock.getOutputStream();
+        // os.write("GET /index.html HTTP/1.0\r\n".getBytes());
+        // os.write("\r\n".getBytes());
+        // os.flush();
+        // InputStreamReader isr = new InputStreamReader(is);
+        // int i = isr.read();
+        // while (i!=-1){
+        //     System.out.print((char)i);
+        //     i = isr.read();
+        // }
+        // sock.close();
+        
+        // 8-3
+        System.out.println("起動完了");
+        ServerSocket svSock = new ServerSocket(39648);
+        Socket sock = svSock.accept();
+        System.out.println(sock.getInetAddress()+"から接続");
+        sock.getOutputStream().write("WELCOME".getBytes());
+        sock.getOutputStream().flush();
+        sock.close();
+
     }
 }
