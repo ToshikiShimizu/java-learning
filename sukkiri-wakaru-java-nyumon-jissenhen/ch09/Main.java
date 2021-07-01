e0da50c3a65c641d456d6a6960f3e0912aaf378f
清水俊樹 <tamreff3290@gmail.com>
2021-07-01 15:14:36 +0900

    code9-2

--- sukkiri-wakaru-java-nyumon-jissenhen/ch09/Main.java
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch09/Main.java
@@ -13,6 +13,19 @@ public class Main{
         try {
             con = DriverManager.getConnection("jdbc:h2:~/rpgdb");
 
+            // code9-2
+            PreparedStatement pstmt = con.prepareStatement("DELETE FROM MONSTERS WHERE HP <= ? OR NAME = ?");
+            pstmt.setInt(1, 10);
+            pstmt.setString(2, "ゾンビ");
+            int r = pstmt.executeUpdate();
+            if (r!=0){
+                System.out.println(r + "件のモンスターを削除しました");
+            }else{
+                System.out.println("該当するモンスターはありませんでした");
+            }
+            pstmt.close();
+            //
+
         } catch (SQLException e){
             e.printStackTrace();
         } finally {
