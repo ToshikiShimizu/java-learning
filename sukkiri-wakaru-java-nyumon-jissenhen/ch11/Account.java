227cdac2aabc04e9700c8dba42d7b4d0afb04135
清水俊樹 <tamreff3290@gmail.com>
2021-07-02 11:51:57 +0900

    code11-1

---
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch11/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    String owner;
+    int zandaka;
+    public void Account(String owner, int zandaka){
+        owner = owner;
+        zandaka = zandaka;
+        public void transfer(Account dest, int amount{
+            dest.zandaka += amount;
+            zandaka -= amount;
+        }
+    }
+}
