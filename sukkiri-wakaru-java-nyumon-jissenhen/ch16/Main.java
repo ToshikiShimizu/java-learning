4c4660100dcdb4bcd94ac2758e5a8f055798d417
Toshiki Shimizu <tamreff3290@gmail.com>
2021-07-03 11:38:05 +0900

    code16-3

--- sukkiri-wakaru-java-nyumon-jissenhen/ch16/Main.java
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch16/Main.java
@@ -4,9 +4,14 @@ public class Main{
     public static void main(String[] args){
     System.out.println("止めるには「STOP」を入力してください");
     System.out.println("カウントダウンを開始します");
-    Thread t = new PrintingThread();
+    PrintingThread t = new PrintingThread();
     t.start();
     String input = new Scanner(System.in).nextLine();
-    System.out.println("入力文字列：" + input);
-    System.out.println("停止処理は未作成です"); }
+    if (input.equals("STOP")){
+        t.stopReq.set(true);
+    }
+    try {
+        t.join();
+    } catch (InterruptedException e) {;}
+}
 }
