542d6d31b8d6614e8ca73cf69c943dc70a909480
Toshiki Shimizu <tamreff3290@gmail.com>
2021-07-03 10:15:29 +0900

    ch15

---
+++ sukkiri-wakaru-java-nyumon-jissenhen/ch15/practice/MyLogger.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public final class MyLogger {
+    private static MyLogger theInstance;
+    private FileWriter writer;
+    private MyLogger() {
+        try {
+            this.writer = new FileWriter("./dummylog.txt");
+        } catch (IOException e) {}
+    }
+    
+    public void log (String msg) throws IOException{
+        this.writer.write(msg);
+    }
+    public static MyLogger getInstance(){
+        if (theInstance == null) theInstance = new MyLogger();
+        return theInstance;
+    }
+}
